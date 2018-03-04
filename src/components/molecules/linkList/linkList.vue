645ed23fc38ff3e1e814b1381b1fa7670c860cf4
pascal.korn <pascal.korn@meinestadt.de>
2018-03-04 18:51:55 +0100

    Linklist and headlines

---
+++ src/components/molecules/linkList/linkList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul class="m-linkList">
+        <li class="m-linkList__item"
+            v-for="link in links"
+            v-bind:key="link.href">
+                <TextLink
+                    v-bind:link="link"
+                ></TextLink>
+        </li>
+    </ul>
+</template>
+
+<script>
+import TextLink from '../../atoms/links/textLinks/textLinks'
+export default {
+  name: 'LinkList',
+  props: ['links'],
+  components: {
+    TextLink
+  }
+}
+</script>
+
+<style>
+
+</style>
